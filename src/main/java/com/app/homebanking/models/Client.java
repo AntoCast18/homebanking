6b2744c5f0a4b873377ee6bca7493b8c045de371
AntoCast18 <101275905+AntoCast18@users.noreply.github.com>
2023-08-04 13:28:35 -0300

    first commit

---
+++ src/main/java/com/app/homebanking/models/Client.java
@@ -0,0 +1,55 @@
+package com.app.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    public Client() {
+    }
+    public Client(String nombre, String apellido, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String toString() {
+        return nombre+ " " + apellido;
+    }
+
+
+}
